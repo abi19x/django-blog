923813a2ca1e9ff422ed5311a507146461d8220d
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-03-11 00:45:20 +0000

    Creating the database

---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_yxJ5bzhHRU1r@ep-ancient-truth-a2qb0i8u.eu-central-1.aws.neon.tech/gift_grief_spoof_515906"
+)
