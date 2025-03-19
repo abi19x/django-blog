e089472105dbfeda66cc3c4a0086d005d62eb6ac
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-03-19 23:24:18 +0000

    event post added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+#Event list post
+from .models import Event
 
 # Create your views here.
 
@@ -31,4 +33,20 @@ def post_detail(request, slug):
         request,
         "blog/post_detail.html",
         {"post": post},
+    )
+
+#Events list post
+class EventsList(generic.ListView):
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+def event_detail(request, event_id):
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
     )
