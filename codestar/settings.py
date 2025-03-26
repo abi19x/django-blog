db57478ea94ebad3f5f5309e42dc1ffb007bca81
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-03-26 21:19:00 +0000

    The form POST handle created

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,8 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '8000-abi19x-djangoblog-vkv0g423ihf.ws-eu118.gitpod.io',
     '.herokuapp.com',
-    '8000-abi19x-djangoblog-ahuaqzck8bn.ws-eu118.gitpod.io',
-    '8000-abi19x-djangoblog-195w24jhwaj.ws-eu118.gitpod.io'
+    '8000-abi19x-djangoblog-z6b9ewke55f.ws-eu118.gitpod.io'
 ]
 
 
@@ -113,8 +112,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
     "https://*.herokuapp.com",
-    "https://8000-abi19x-djangoblog-ahuaqzck8bn.ws-eu118.gitpod.io",    
-    "https://8000-abi19x-djangoblog-195w24jhwaj.ws-eu118.gitpod.io",
+    "https://8000-abi19x-djangoblog-z6b9ewke55f.ws-eu118.gitpod.io",
 ]
 
 # Password validation
