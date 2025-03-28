13c813e639c2aaed103b62c236d3ce856132d6b6
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-03-28 17:46:03 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-csq9vd4p+hz$2@1bfmf4obb=87b&@d#1*xoq4ozaeqru+t6p+t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-abi19x-djangoblog-vkv0g423ihf.ws-eu118.gitpod.io',
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
--- env.py
+++ env.py
@@ -4,4 +4,4 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_yxJ5bzhHRU1r@ep-ancient-truth-a2qb0i8u.eu-central-1.aws.neon.tech/gift_grief_spoof_515906"
 )
 os.environ.setdefault(
-    "CLOUDINARY_URL", "CLOUDINARY_URL=cloudinary://@dk8wii4kl")
+    "CLOUDINARY_URL", "cloudinary://@dk8wii4kl")
