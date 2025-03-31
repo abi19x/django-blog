f8407dc929c9fb555abf6a0202e71e92987eba7f
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-03-31 20:52:30 +0000

    automative tests added to the about app

---
+++ about/test_forms.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from .forms import CollaborateForm
+
+
+class TestCollaborateForm(TestCase):
+
+    def test_form_is_valid(self):
+        """ Test for the name field"""
+        form = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(form.is_valid(), msg="Name was not provided, but form is valid")
+    
+    def test_email_is_required(self):
+        """Test for the 'email' field"""
+        form = CollaborateForm({
+            'name': 'Abel',
+            'email': '',
+            'message': 'Hello!'
+        })
+        self.assertFalse(form.is_valid(), msg="Email was not provided, but the form is valid")
+    
+    def test_message_is_required(self):
+        """Test for the 'message' field"""
+        form = CollaborateForm({
+            'name': 'Abel',
+            'email': 'test01@gmail.com',
+            'message': ''
+        })
+        self.assertFalse(form.is_valid(), msg="Message was not provided, but the form is valid")
